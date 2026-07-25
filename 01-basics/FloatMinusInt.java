552d7bc6225e3e9b69671d8d8d151a9717d36f9a
Brian <brayo2933@gmail.com>
2026-07-25 11:29:00 +0300

    Add FloatMinusInt exercise

---
+++ 01-basics/FloatMinusInt.java
@@ -0,0 +1,10 @@
+public class FloatMinusInt {
+
+    public double subtractIntFromDoubleAndReturnDouble(double floating, int integer) {
+        return floating- integer ;
+    }
+
+    public int subtractIntFromDoubleAndReturnInt(double floating, int integer) {
+        return(int) floating-integer ;
+    }
+}
