5b7da1c7149353925aa459cf9b79491e5d6b6793
Brian <brayo2933@gmail.com>
2026-07-25 11:43:09 +0300

    Add GreetingCard exercise

---
+++ 02-strings/GreetingCard.java
@@ -0,0 +1,7 @@
+public class GreetingCard {
+
+    public String createGreeting(String recipientName, String occasion){
+        return "Happy" + occasion + "!\nTo:" + recipientName + "\nBest wishes!";
+    }
+    
+}
